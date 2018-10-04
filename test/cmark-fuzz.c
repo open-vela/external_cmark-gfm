70ddb299adf37e529abaf0afe7171e1336ce12d2
liujinye <liujinye@xiaomi.com>
2018-10-04 15:40:26 +0800

    Update spec.txt.

--- test/cmark-fuzz.c
+++ test/cmark-fuzz.c
@@ -3,28 +3,24 @@
 #include "cmark.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  struct __attribute__((packed)) {
-    int options;
-    int width;
-  } fuzz_config;
-
-  if (size >= sizeof(fuzz_config)) {
-    /* The beginning of `data` is treated as fuzzer configuration */
-    memcpy(&fuzz_config, data, sizeof(fuzz_config));
+  int options = 0;
+  if (size > sizeof(options)) {
+    /* First 4 bytes of input are treated as options */
+    int options = *(const int *)data;
 
     /* Mask off valid option bits */
-    fuzz_config.options &= (CMARK_OPT_SOURCEPOS | CMARK_OPT_HARDBREAKS | CMARK_OPT_SAFE | CMARK_OPT_NOBREAKS | CMARK_OPT_NORMALIZE | CMARK_OPT_VALIDATE_UTF8 | CMARK_OPT_SMART);
+    options = options & (CMARK_OPT_SOURCEPOS | CMARK_OPT_HARDBREAKS | CMARK_OPT_SAFE | CMARK_OPT_NOBREAKS | CMARK_OPT_NORMALIZE | CMARK_OPT_VALIDATE_UTF8 | CMARK_OPT_SMART);
 
     /* Remainder of input is the markdown */
-    const char *markdown = (const char *)(data + sizeof(fuzz_config));
-    const size_t markdown_size = size - sizeof(fuzz_config);
-    cmark_node *doc = cmark_parse_document(markdown, markdown_size, fuzz_config.options);
+    const char *markdown = (const char *)(data + sizeof(options));
+    const size_t markdown_size = size - sizeof(options);
+    cmark_node *doc = cmark_parse_document(markdown, markdown_size, options);
 
-    free(cmark_render_commonmark(doc, fuzz_config.options, fuzz_config.width));
-    free(cmark_render_html(doc, fuzz_config.options));
-    free(cmark_render_latex(doc, fuzz_config.options, fuzz_config.width));
-    free(cmark_render_man(doc, fuzz_config.options, fuzz_config.width));
-    free(cmark_render_xml(doc, fuzz_config.options));
+    free(cmark_render_commonmark(doc, options, 80));
+    free(cmark_render_html(doc, options));
+    free(cmark_render_latex(doc, options, 80));
+    free(cmark_render_man(doc, options, 80));
+    free(cmark_render_xml(doc, options));
 
     cmark_node_free(doc);
   }
