1e76407f53f5fd2c4b7dd998ca537c13ed9c1f5f
liujinye <liujinye@xiaomi.com>
2019-03-17 10:23:47 +0800

    Disallow unescaped ( in parenthesized link title.
    In accord with spec change.

--- src/cmark.c
+++ src/cmark.c
@@ -1,18 +1,14 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdio.h>
-#include "registry.h"
 #include "node.h"
 #include "houdini.h"
-#include "cmark-gfm.h"
+#include "cmark.h"
 #include "buffer.h"
 
-cmark_node_type CMARK_NODE_LAST_BLOCK = CMARK_NODE_FOOTNOTE_DEFINITION;
-cmark_node_type CMARK_NODE_LAST_INLINE = CMARK_NODE_FOOTNOTE_REFERENCE;
+int cmark_version() { return CMARK_VERSION; }
 
-int cmark_version() { return CMARK_GFM_VERSION; }
-
-const char *cmark_version_string() { return CMARK_GFM_VERSION_STRING; }
+const char *cmark_version_string() { return CMARK_VERSION_STRING; }
 
 static void *xcalloc(size_t nmem, size_t size) {
   void *ptr = calloc(nmem, size);
@@ -32,15 +28,7 @@ static void *xrealloc(void *ptr, size_t size) {
   return new_ptr;
 }
 
-static void xfree(void *ptr) {
-  free(ptr);
-}
-
-cmark_mem CMARK_DEFAULT_MEM_ALLOCATOR = {xcalloc, xrealloc, xfree};
-
-cmark_mem *cmark_get_default_mem_allocator() {
-  return &CMARK_DEFAULT_MEM_ALLOCATOR;
-}
+cmark_mem DEFAULT_MEM_ALLOCATOR = {xcalloc, xrealloc, free};
 
 char *cmark_markdown_to_html(const char *text, size_t len, int options) {
   cmark_node *doc;
@@ -48,7 +36,7 @@ char *cmark_markdown_to_html(const char *text, size_t len, int options) {
 
   doc = cmark_parse_document(text, len, options);
 
-  result = cmark_render_html(doc, options, NULL);
+  result = cmark_render_html(doc, options);
   cmark_node_free(doc);
 
   return result;
