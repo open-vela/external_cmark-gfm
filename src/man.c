aaf9b0365587cbdefc41237ebca5d313dd69384a
openvela-robot <openvela-robot@xiaomi.com>
2023-04-03 15:39:13 +0800

    Merge pull request #310 from huven/prototype-warning
    Fix strict prototype clang warning

--- src/man.c
+++ src/man.c
@@ -113,6 +113,7 @@ static int S_render_node(cmark_renderer *renderer, cmark_node *node,
     break;
 
   case CMARK_NODE_LIST:
+    renderer->list_number = cmark_node_get_list_start(node);
     break;
 
   case CMARK_NODE_ITEM:
@@ -122,7 +123,7 @@ static int S_render_node(cmark_renderer *renderer, cmark_node *node,
       if (cmark_node_get_list_type(node->parent) == CMARK_BULLET_LIST) {
         LIT("\\[bu] 2");
       } else {
-        list_number = cmark_node_get_item_index(node);
+        list_number = renderer->list_number++;
         char list_number_s[LIST_NUMBER_SIZE];
         snprintf(list_number_s, LIST_NUMBER_SIZE, "\"%d.\" 4", list_number);
         LIT(list_number_s);
